98f00ac9f79d46875047cb5666e66e8c01cd86f4
Leonardo Caloni Munduruca <leomunduruca@gmail.com>
2025-05-08 17:17:06 -0300

    detalhes arrumados:

--- web/src/pages/Chatbot.jsx
+++ web/src/pages/Chatbot.jsx
@@ -1,5 +1,28 @@
-// Chatbot.jsx
-export default function Chatbot() {
-  // Adicione "default"
-  return <div>Componente do Chatbot</div>;
+import React from "react";
+import { Link } from "react-router-dom";
+
+import chatbotimagem from "../assets/chatbot.jpg";
+
+import "./Chatbot.css";
+
+function Chatbot() {
+  return (
+    <>
+      <div className="foto-principal">
+        <img src={chatbotimagem} className="foto-bot" alt="chatbot" />
+        <div className="escurecedor"></div>
+        <div className="texto-img-principal">
+          <h1>
+            Agendamentos
+            <br />
+            e atendente
+            <br />
+            virtual
+          </h1>
+        </div>
+      </div>
+    </>
+  );
 }
+
+export default Chatbot;
